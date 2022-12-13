4570565e6072a39861a9a87a753c36236ede5186
mwmilindadev <mwmilinda@gmail.com>
2022-12-13 06:46:19 +0530

    Added Sream

--- src/FP01Functional.java
+++ src/FP01Functional.java
@@ -2,15 +2,31 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args) {
-        
-        printAllNumbersInFunctional(List.of(12, 15, 9, 2, 4, 5, 3, 1));
+
+        List<Integer> numbers = List.of(12, 15, 9, 2, 4, 5, 3, 1);
+        //printAllNumbersInFunctional(numbers);
+        printAllEvenNumbersInFunctional(numbers);
     }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static void printAllEvenNumbersInFunctional(List<Integer> numbers) {
+        numbers
+                .stream()
+                .filter(FP01Functional::isEven)//filter only even numbers
+                .forEach(System.out::println);//Method reference
+    }
+
     private static void print(int number) {
         System.out.println(number);
     }
+
     private static void printAllNumbersInFunctional(List<Integer> numbers) {
         numbers.stream().forEach(FP01Functional::print);
 
     }
+
 
 }
--- src/FP01Structured.java
+++ src/FP01Structured.java
@@ -2,7 +2,19 @@ import java.util.List;
 
 public class FP01Structured {
     public static void main(String[] args) {
-        printAllNumbersStructure(List.of(12, 15, 9, 2, 4, 5, 3, 1));
+        List<Integer> numbers = List.of(12, 15, 9, 2, 4, 5, 3, 1);
+        //printAllNumbersStructure(numbers);
+        printAllEvenNumbersStructure(numbers);
+
+    }
+
+    private static void printAllEvenNumbersStructure(List<Integer> numbers) {
+
+        for (int number:numbers){
+            if(number%2==0){
+                System.out.println(number);
+            }
+        }
     }
 
     private static void printAllNumbersStructure(List<Integer> numbers) {
